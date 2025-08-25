60359704190a4db6a6d611f0b6ef892384f09e7b
Nishi Shah <nishahh26@gmail.com>
2025-08-25 22:11:07 +0530

    Create que31.c

---
+++ que31.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() 
+{
+    int sum=0;
+    for(int n=2; n<=500; n=n+1) {
+        int flag=1;
+        for(int i=2; i<=n/2; i=i+1) {
+            if(n % i == 0) {flag=0;break;}
+        }
+        if(flag) {sum += n;}
+    }
+    printf("Sum of primes = %d\n", sum);
+    return 0;
+}
