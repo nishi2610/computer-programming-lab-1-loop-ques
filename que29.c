94e2906c3e5c23edbf4f2ff3847faf6e42608550
Nishi Shah <nishahh26@gmail.com>
2025-08-25 22:05:40 +0530

    Create que29.c

---
+++ que29.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() 
+{
+    int n, flag=1;
+    printf("Enter number:");
+    scanf("%d", &n);
+    if(n < 2) {flag = 0;}
+    for(int i=2; i<=n/2; i=i+1) {
+        if(n % i == 0) {flag = 0; break;}
+    }
+    if(flag=1) {printf("Prime\n");}
+    else {printf("Not Prime\n");}
+    return 0;
+}
