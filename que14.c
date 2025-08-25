d96a16f20d4e761f2f804be54189e339589b8036
Nishi Shah <nishahh26@gmail.com>
2025-08-25 21:14:31 +0530

    Update que14.c

--- que14.c
+++ que14.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-int main() 
-{
-    int a, sum=0;
-    for(int i=1; i<=10; i=i+1) {
-        printf("Enter number %d: ", i);
-        scanf("%d", &a);
-        sum += a;
-    }
-    printf("Sum = %d\nMean = %.2f", sum, sum/10.0);
+int main() {
+    int num, sum = 0;
+    float mean;
+    for(int i = 1; i <= 10; i=i+1) {
+        printf("Enter number %d:", i);
+        scanf("%d", &num);
+        sum += num;}
+    mean = sum / 10.0;
+    printf("Sum = %d\n", sum);
+    printf("Mean = %.2f\n", mean);
     return 0;
 }
